7f540e92efe57b1f61d4aace47083c626215faa4
Jose Luis R. M <jlroviramartin@gmail.com>
2018-07-31 10:04:14 +0200

    no message

---
+++ geometry-core-builder/src/main/java/templatesforvectors/Run.java
@@ -0,0 +1,18 @@
+package templatesforvectors;
+
+/**
+ *
+ * @author joseluis
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        //new TupleTemplates().execute();
+        //new VectorTemplates().execute();
+        //new PointTemplates().execute();
+        //new ColorTemplates().execute();
+        //new MatrixTemplates().execute();
+
+        new TupleTemplates2().execute();
+    }
+}
